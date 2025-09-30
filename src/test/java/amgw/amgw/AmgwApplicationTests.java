d0a4537019cca42943292c4fc1eb7dcf490e8c19
torye2 <rkdgywh6343@gmail.com>
2025-09-30 14:46:59 +0900

    base

---
+++ src/test/java/amgw/amgw/AmgwApplicationTests.java
@@ -0,0 +1,13 @@
+package amgw.amgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmgwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
