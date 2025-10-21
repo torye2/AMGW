999eba8fb309a9d39104915dc71a542dc96d148a
wsw8848 <ssh-ed25519 AAAAC3NzaC1IZDI1NTE5AAAAIHf1nCdlb88nORRZwihz9zoufp7nSbz+uc2aL1VPJFv8 icecream8347@gmail.com>
2025-10-21 11:45:52 +0900

    ai챗봇 기능 추가

--- src/main/resources/db/migration/V1__init_users_and_roles.sql
+++ src/main/resources/db/migration/V1__init_users_and_roles.sql
@@ -14,6 +14,17 @@ CREATE TABLE users (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- 기본 관리자 계정
-INSERT INTO users (username, password, name, email, role)
-VALUES ('admin', '$2a$10$z4vK2T3XqKfLzvZ1t7cWOeZ7qQCM.tYkq5xP1m80xTxZKxQzvTxJ6', '관리자', 'admin@example.com', 'ADMIN');
+INSERT INTO users (username, password, name, email, role) VALUES
+('admin', '$2a$10$z4vK2T3XqKfLzvZ1t7cWOeZ7qQCM.tYkq5xP1m80xTxZKxQzvTxJ6', '관리자', 'admin@example.com', 'ADMIN'),
+('rkdgywh', '$2a$10$4Borv55PFh4G7/Mg8WlPpOrhIQny/6JoseJHNZjaXHLbtNyMUWHl2', '강효조', 'rkdgywh6343@naver.com', 'ADMIN'),
+('rkdgyqls', '$2a$10$rnDCwVtIAFE4AYwwkwKFgOQe6wNz4VewxnUU2kFZShZsyayz/utZu', '강효빈', 'gyqls6343@naver.com', 'EMPLOYEE'),
+('sw010111', '$2a$10$GucvYfH1peyxf71Sd468K.RxK6CzPJvVr6ho3w0WbnAAPOBmmtFBi', '우승우', 'sws8347@gmail.com', 'ADMIN'),
+('leeoksou', '$2a$10$j08/XdL0sf/XSN8g/w8FpOxxjGUWPdN0lFsmzcFFoQmq9hrGQ5KKK', '이옥순', 'rkdgywh6343@gmail.com', 'EMPLOYEE'),
+('1111', '$2a$10$E8Vn5GP4UxzP4Cc/vQWAuu1Qc1zm.YTSfjZDT1tqRzaN.vBKXg8ha', '홍길동', '1111@naver.com', 'ADMIN'),
+('2222', '$2a$10$PTSQYACXyr6BwJlOmWQMGujbkWiCo9YC.Mxmpo3hFI8/dYiFTQSVO', '홍이동', '2222@naver.com', 'ADMIN'),
+('3333', '$2a$10$j8tySYuf9vYUhvRoMQ16puw.HAHnyxJ/SRm40OclwiM4k49IxuTlK', '홍삼동', '3333@naver.com', 'ADMIN'),
+('emailtest', '$2a$10$Ly0K.on2g61OO3xrVuQEb.N5PvzDZZXSXxLa.5pTqhCyE/wlr7I0S', '이메일', 'test@email.local', 'EMPLOYEE'),
+('emailtestuser', '$2a$10$FKUn4KBG.xIW9qcpB/5/Aufb7Y95azjLYf3noRUw0LpRJERpEHp3K', '임해일', 'emailtest@test.local', 'EMPLOYEE'),
+('email', '$2a$10$G1WKFQfXnLB2ngp8Od23Xe2o1icHrI9NjYTKNz6zVbjGfg.RcqmSu', 'email', 'testemail@mail.local', 'EMPLOYEE');
+
 -- 위 password는 'admin123'의 BCrypt 예시
